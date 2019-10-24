de97da38878644b6c7b5b2e33447631b1dee4254
alex80sboy <56180716+alex80sboy@users.noreply.github.com>
2019-10-24 14:09:43 -0500

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-$('.gallery img').on('click', function(){
-    let imageAddres = $(this).attr('scr');
-    $('.showroom img').attr('scr', imageAddres);
-    $('.showroom img').removeClass('hidden');
-    $('.showroom img').addClass('hidden');
-})
